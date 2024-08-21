b387472be9a8d2cb8aa03fda46bdfe720419b98b
Riddhima Agarwal <agarwalriddhima5@gmail.com>
2024-08-21 20:03:31 +0530

    workshop

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Keyboard from './component/Keyboard/Keyboard';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Keyboard/>}></Route>
+        </Routes>
+        </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
